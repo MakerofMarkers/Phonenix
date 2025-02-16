b0cbc15288ed9e492a0ddaa04e8d4d82b23cd140
Olivia Xu <olivialx@stanford.edu>
2025-02-15 16:53:41 -0800

    combined speechtotext.py and transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,5 +1,5 @@
 import whisper
-from wordanalysis import string_to_syllables
+from wordanalysis import string_to_syllables, get_diff
 
 # Ignore the annoying warnings :(
 import warnings
@@ -12,25 +12,36 @@ def transcribe(file_name):
     result = model.transcribe(file_name)
     return result["text"]
 
-# This function takes in the path to an audio file and returns the speech
-# as a list of syllables in IPA
-def process_audio(file_name):
-    # Transcribe audio file
-    transcribed_text = transcribe(file_name)
+# This function takes in the path to an audio file and a target sentence, and
+# transcribes the audio file with whisper. Then, the syllables in the target
+# sentence that are mispronounced are returned by calling `compare_phonemes`.
+def analyze_pronunciation(audio_path, target_sentence):
+    # Transcribe audio
+    transcribed_sentence = transcribe(audio_path)
     
-    # Convert transcribed text to syllables
-    syllables = string_to_syllables(transcribed_text)
-    
-    return syllables
+    # Compare syllables in two sentences
+    incorrect_syllables = get_diff(transcribed_sentence, target_sentence)
+
+    return incorrect_syllables
 
 # TESTING
-def testing():
-    s = transcribe("speechtestwrong.m4a")
-    target = transcribe("speechtest.wav")
-    print("Speech to text:", s)
-    syllables = process_audio("speechtestwrong.m4a")
-    print("Syllables:", syllables)
-    print(get_diff(s, target))
+# def testing():
+#     s = transcribe("speechtestwrong.m4a")
+#     target = transcribe("speechtest.wav")
+#     print("Speech to text:", s)
+#     syllables = process_audio("speechtestwrong.m4a")
+#     print("Syllables:", syllables)
+#     print(get_diff(s, target))
 
-if __name__ == "__main__":
-    testing()
+#     print("\nTESTING PART 2\n")
+#     audio_file = "speechtest.wav"
+#     target = "Kids are running by the fence"
+#     differences = analyze_pronunciation(audio_file, target)
+
+#     print(differences)
+    
+#     for target_word, pronounced_word in differences.items():
+#         print(f"Word '{target_word}' pronounced as '{pronounced_word}'")
+
+# if __name__ == "__main__":
+#     testing()
